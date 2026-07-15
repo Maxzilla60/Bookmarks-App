98fe155e1cc731a374061b04d9a51bdcffb58b63
Maxzilla60 <maxpower98@hotmail.com>
2026-07-15 18:58:40 +0200

    👷 Improve Playwright Github Action
    https://endform.dev/blog/playwright-github-actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
 	fullyParallel: true,
 	forbidOnly: !!process.env['CI'],
 	retries: process.env['CI'] ? 2 : 0,
-	workers: process.env['CI'] ? 1 : undefined,
-	reporter: 'html',
+	workers: process.env['CI'] ? '50%' : undefined,
+	reporter: [['html'], ['github']],
 	use: {
 		baseURL: 'http://localhost:5173',
 		trace: 'on-first-retry',
